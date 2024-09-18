e9080fe2d4263226aa8dde183be5c1cc02852953
abusaleh123 <abusalehnoor4103@gmail.com>
2024-09-19 05:40:48 +0600

    added home event handler

--- home.js
+++ home.js
@@ -1,1 +1,17 @@
-console.log('home added');
+document.getElementById('last-add-money-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    
+    const addMoneyInput = document.getElementById('add-money-input').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(addMoneyInput);
+    console.log(pinNumber);
+
+    if(pinNumber === '1234'){
+        console.log('You are Logged In');
+        // window.location.href = '';
+    }
+    else{
+        alert('wrong Pin Number')
+    }
+   
+})
